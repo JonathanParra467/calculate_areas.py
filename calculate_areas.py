8ce3cd26578933ed39956d19a5dc4375d3ee9250
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-09-21 18:16:40 -0500

    Add files via upload

---
+++ calculate_areas.py
@@ -0,0 +1,12 @@
+def square(sides):
+    print("The squares area is " + sides + " square units.")
+
+
+square("86")
+
+
+def circle(radius):
+    print("The circles area is " + radius + " square units.")
+
+
+circle("37")
